20022d0a45c8f7f9389a5c19684e3e26979e205a
Nick Hudkins <nhudkins@amazon.com>
2024-03-16 16:33:54 -0400

    FIXED

--- auth.ts
+++ auth.ts
@@ -1,11 +1,11 @@
 import path from "path";
-import { authenticate } from "@google-cloud/local-auth";
 import { google } from "googleapis";
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"];
 
 const CREDENTIALS_PATH = path.join(process.cwd(), "credentials.json");
+const IMPERSONATE = "nick@nickhudkins.com";
 /**
  * Load or request or authorization to call APIs.
  *
@@ -14,5 +14,8 @@ export async function authorize() {
   return new google.auth.GoogleAuth({
     scopes: SCOPES,
     keyFile: CREDENTIALS_PATH,
+    clientOptions: {
+      subject: IMPERSONATE,
+    },
   });
 }
